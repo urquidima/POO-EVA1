54cfdb3653066ab4f6660e5cdf425a4a0df67424
urquidima <65150816+urquidima@users.noreply.github.com>
2022-02-11 14:01:25 -0700

    Add files via upload

---
+++ EVA_1_HOLAMUNDO.JAVA
@@ -0,0 +1,11 @@
+//palabras azules: palabras reservadas
+//palabras amarillas: son identificadores para variables, clases etc.
+//{}: delimitadores
+//[]: son arreglos
+
+
+public class EVA_1_HOLAMUNDO{//cuerpo de la clase
+    public static void main(String[] args) {//funcion
+        System.out.println("hola mundo");
+    }
+}
